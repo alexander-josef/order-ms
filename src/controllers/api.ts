abcc005bf157f344cc4de89f5449ed8bc7f7d0ae
Alexander Josef <alexander.josef@netcetera.com>
2019-11-24 14:12:29 +0100

    adding openapi description / title for service

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,5 +2,5 @@ import { NextFunction, Request, Response } from 'express'
 import {formatOutput} from '../utility/orderApiUtility'
 
 export let getApi = (req: Request, res: Response, next: NextFunction) => {
-  return formatOutput(res, { title: 'Order API' }, 200)
+  return formatOutput(res, { title: 'Order Microservice' }, 200)
 }
